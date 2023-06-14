710f7cbbab795fd084c3133e45f73560c68bf324
Juan <juampi.parzybal@gmail.com>
2023-06-14 18:37:53 -0300

    okey

--- Client/src/app/page.js
+++ Client/src/app/page.js
@@ -1,17 +1,18 @@
 'use client'
-import Link from "next/link";
-import { changeName } from "../redux/slice/slice";
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+
 import Landing from "@/pages/Landing";
+import Home from "@/pages/Home";
+import { Router } from "next/router";
 
 export default function App() {
 
-  return (
-    <div>
 
-      <Landing></Landing>
-      
-    </div>
+  return (
+      <div>
+        
+          <Home></Home>
+          <Landing></Landing>
+
+      </div>
   )
 }
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,9 +1,21 @@
+'use client'
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { axiosAllProducts } from "../redux/slice/productSlice"
+import CreateProduct from "./CreateProduct/CreateProduct";
 import style from "./styles/Home/Home.module.css"
 
 const Home = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(axiosAllProducts())
+    })
+
     return (
         <div className={style.body}>
             <h1>Esto es el Home</h1>
+            <CreateProduct></CreateProduct>
         </div>
     )
 }
