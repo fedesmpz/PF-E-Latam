2b2372915b9c27fccacfb6c99175622b5e253643
CristianR1992 <rodriguez.cristianezequiel@hotmail.com>
2023-06-14 09:42:10 +0100

    get/Users/:id

--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -2,8 +2,10 @@ const { Router } = require('express');
 const router = Router();
 const { getApiInfoHandler } = require('../Handlers/index')
 const { getByCountryHandler } = require("../Handlers/getByCountry")
+const {getUserByIdHandler} = require("../Handlers/getUserById")
 
 router.get('/products', getApiInfoHandler);
 router.get("/products/:id", getByCountryHandler)
+router.get("/users/:id", getUserByIdHandler)
 
 module.exports = router;
