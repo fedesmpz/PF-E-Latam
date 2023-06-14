6002011098791b0fa8303af1911845ae682a9616
camilobit <camiloacevedovargas@gmail.com>
2023-06-13 23:39:37 -0500

    controller, handler y route del filtrado por categorias

--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -2,8 +2,10 @@ const { Router } = require('express');
 const router = Router();
 const { getApiInfoHandler } = require('../Handlers/index')
 const { getByCountryHandler } = require("../Handlers/getByCountry")
+const getByCategoryHandler = require("../Handlers/getByCategory")
 
 router.get('/products', getApiInfoHandler);
-router.get("/products/:id", getByCountryHandler)
+router.get("/products/:id", getByCountryHandler);
+router.get("/products/:countryId/:category", getByCategoryHandler);
 
 module.exports = router;
