177afedcf123d40dcb528a3e83dd399b3b5fd734
CristianR1992 <rodriguez.cristianezequiel@hotmail.com>
2023-06-14 13:48:16 +0100

    agregado return antes del res

--- Server/Handlers/getProductById.js
+++ Server/Handlers/getProductById.js
@@ -4,7 +4,7 @@ const getProductByIdHandler =  async (req,res)=>{
     const {id} = req.params
   try {
     const product = await getProductById (id);
-    res.status(200).json(product)
+    return  res.status(200).json(product)
 
   } catch (error) {
       res.status(400).json({error:error.message});
