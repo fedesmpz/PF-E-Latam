a191b80acd43b0a871ecf8edb0b76826ed6ac59e
CristianR1992 <rodriguez.cristianezequiel@hotmail.com>
2023-06-14 13:32:23 +0100

    post/register

--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -2,8 +2,10 @@ const { Router } = require('express');
 const router = Router();
 const { getApiInfoHandler } = require('../Handlers/index')
 const { getByCountryHandler } = require("../Handlers/getByCountry")
+const {registerHandler} = require("../Handlers/register")
 
 router.get('/products', getApiInfoHandler);
 router.get("/products/:id", getByCountryHandler)
+router.post("/register", registerHandler)
 
 module.exports = router;
