7497dac5a5ee4bc4caa9cc4e0d4a7dd3ba97994e
Violeta <holasoyviole@gmail.com>
2023-06-15 11:51:12 -0300

    post de Review funcionando

--- Server/Routes/ratingAndReview.js
+++ Server/Routes/ratingAndReview.js
@@ -2,8 +2,10 @@ const { Router } = require('express');
 const router = Router();
 
 const { getReviewHandler } = require('../Handlers/get/getReview'); 
+const { postReviewHandler } = require('../Handlers/post/postReview');
 
 router.get('/:reviewID', getReviewHandler);
+router.post('/:reviewID', postReviewHandler);
 
 
 module.exports = router;
