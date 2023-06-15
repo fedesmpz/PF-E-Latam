1c809de7b00912aa9d291cc86a2af050af9db31d
GaboYopasa <gyopasaa@gmail.com>
2023-06-15 10:54:59 -0500

    SerachBar

--- Client/src/pages/SearchBar.jsx
+++ Client/src/pages/SearchBar.jsx
@@ -3,18 +3,24 @@ import { useDispatch } from "react-redux";
 import { axiosAllProducts } from "@/redux/slice/productSlice";
 import style from "./styles/SearchBar/SearchBar.module.css";
 
+const validate = (input) => {
+    let errors = {};
+    if (!input.length) errors = "Inserta un producto";
+}
+
 export default function SearchBar() {
     const dispatch = useDispatch();
     const [name, setName] = useState('');
+    const [errors, setErrors] = useState({});
 
-    const handleInputChange = (event) => {
+    const handleChange = (event) => {
         event.preventDefault();
         setName(event.target.value);
+        setErrors(validate(event.target.value))
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        if (!name.length) return alert('Insert a product');
         dispatch(axiosAllProducts(name));
     };
 
@@ -22,8 +28,9 @@ export default function SearchBar() {
         <div className={style.container}>
             <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"/>
             
-            <input type="text" placeholder="Search a product..." onChange={handleInputChange} value={name}/>
+            <input type="text" placeholder="Busca un producto..." onChange={handleChange} value={name}/>
             <button className={style.btn} type="submit" onClick={event => {handleSubmit(event); setName('')}}>{<i class="fa fa-search"></i>}</button>
+            {errors && <p className={style.error}>{errors}</p>}
         </div>
     )
 };
