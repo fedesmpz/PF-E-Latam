3340478a58350232b2480747e2662f86b9356d45
Victoria-Barrientos <viictoriabarrientos@gmail.com>
2023-06-15 09:02:32 -0300

    Change on country changes currency_id

--- Server/Controllers/put/updateUser.js
+++ Server/Controllers/put/updateUser.js
@@ -1,18 +1,35 @@
-const {User}=require("../../db")
+const {User}=require("../../db");
+const {Cart}=require("../../db");
 
-const updateUser = async(id,name,surname,email,birth_date,profile_picture,country,city,adress,postal_code,admin,superAdmin)=>{
+const { currencyIdValidator } = require("../../Utilities/currencyIdValidator.js")
+
+const updateUser = async(id,name,surname,email,birth_date,profile_picture,country,city,adress,postal_code)=>{
 
 try {
-    const user = await User.findByPk(id)
+    const user = await User.findByPk(id, {
+        include: Cart
+    })
     if(!user){
-        throw new Error("No se encontro usuario con ese ID para su update")
+        throw new Error("No se encontro usuario con ese ID para su actualizacion")
     }
-    const updateUser = await User.update({
-            name,surname,email,birth_date,profile_picture,country,city,adress,postal_code,admin,superAdmin
-        },{
-            where:{id:id}
-        })
-    
+    let currency_id = null
+    if(user.country !== country) {
+        currency_id = currencyIdValidator(country)
+    };
+    const updatedUser = await User.update({
+            name,surname,email,birth_date,profile_picture,country,city,adress,postal_code
+        },
+        {where:{id:id}}
+    );
+    if (currency_id) {
+        const updatedCart = await Cart.update(
+          {
+            currency_id: currency_id
+          },
+          { where: { userId: id } }
+        );
+    }
+    return "Usuario actulizado correctamente"    
 } catch (error) {
     throw new Error('Error al actualizar el usuario');
 }
