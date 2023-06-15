d2e38d0f5cf9d4e537de8f36b7664f05576ba007
Violeta <holasoyviole@gmail.com>
2023-06-15 13:34:48 -0300

    delete de Review funcionando

---
+++ Server/Controllers/delete/deleteReview.js
@@ -0,0 +1,18 @@
+const { ReviewRating } = require("../../db");
+
+const deleteReview = async (reviewID) => {
+  try {
+    const review = await ReviewRating.findByPk(reviewID);
+
+    if (!review) {
+      throw new Error("No se encontró la reseña por ID");
+    }
+
+    await review.destroy();
+    return { message: "Reseña eliminada exitosamente" };
+  } catch (error) {
+    throw new Error("No se pudo eliminar la reseña");
+  }
+};
+
+module.exports = { deleteReview };
