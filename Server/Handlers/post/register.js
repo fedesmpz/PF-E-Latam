e478e87a8cb52ccf7c82fa045ec76075280ed4bd
Victoria-Barrientos <viictoriabarrientos@gmail.com>
2023-06-15 09:54:28 -0300

    Changed adress for address

--- Server/Handlers/post/register.js
+++ Server/Handlers/post/register.js
@@ -1,9 +1,9 @@
 const {registerUser} = require("../../Controllers/post/registerUser")
 
 const registerHandler= async(req,res)=>{
-    const{id,name,surname,email,birth_date,profile_picture,country,city,adress,postal_code,admin,superAdmin}= req.body
+    const{id,name,surname,email,birth_date,profile_picture,country,city,address,postal_code,admin,superAdmin}= req.body
     try {
-        const register= await registerUser(id,name,surname,email,birth_date,profile_picture,country,city,adress,postal_code,admin,superAdmin)
+        const register= await registerUser(id,name,surname,email,birth_date,profile_picture,country,city,address,postal_code,admin,superAdmin)
         res.status(200).json(register)
     } catch (error) {
         res.status(400).json({ error: error.message });
