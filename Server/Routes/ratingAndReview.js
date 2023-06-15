2a6647cbde38b900f0236919b032497eb7c86c06
Violeta <holasoyviole@gmail.com>
2023-06-15 08:18:55 -0300

    get de Review funcionando

--- Server/Routes/ratingAndReview.js
+++ Server/Routes/ratingAndReview.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 
+const { getReviewHandler } = require('../Handlers/get/getReview'); 
+
+router.get('/:reviewID', getReviewHandler);
 
 
 module.exports = router;
