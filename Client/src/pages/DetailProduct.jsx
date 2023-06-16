f455e11b916dd0bc9b95582b1cc626955cfcb60d
CristianR1992 <rodriguez.cristianezequiel@hotmail.com>
2023-06-16 17:13:41 +0100

    renderizado del detail

--- Client/src/pages/DetailProduct.jsx
+++ Client/src/pages/DetailProduct.jsx
@@ -4,45 +4,60 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux"
 import { useRouter } from "next/router";
 import Providers from "@/redux/provider/Provider"
-const DetailProduct =()=>{
+import { all } from "axios";
+const DetailProduct = () => {
 
-const dispatch = useDispatch()
-const router= useRouter()
-const {id}= router.query
-const {categories}= router.query
-const {countryId}=router.query
-
-
-
-const productDetail = useSelector((state)=>state.products.detail)
-
- useEffect(() => {
-      dispatch(axiosAllProductByCountryCategoryId(id, countryId, categories));
-    // return () => dispatch(cleanDetail())  CREAR PARA LIMPIAR DETALLES
-  }, [dispatch, id, countryId, categories]);
-  console.log(productDetail)
+    const dispatch = useDispatch()
+    const router = useRouter()
+    const { id } = router.query
+    const { categories } = router.query
+    const { countryId } = router.query
 
 
-    return(
+
+    const productDetail = useSelector((state) => state.products.detail)
+
+    useEffect(() => {
+        dispatch(axiosAllProductByCountryCategoryId(id, countryId, categories));
+        // return () => dispatch(cleanDetail())  CREAR PARA LIMPIAR DETALLES
+    }, [dispatch, id, countryId, categories]);
+
+    console.log(productDetail.attributes);
+    const attributes = productDetail.attributes ? JSON.parse(productDetail.attributes) : [];
+
+    const renderedAttributes = attributes.map(attribute => {
+        const attributeName = attribute.name;
+        const attributeValue = attribute.value;
+        return `${attributeName}: ${attributeValue}`;
+    });
+
+
+    console.log(renderedAttributes)
+
+    return (
         <div>
-           {
-            productDetail?.title ?(
-                <>
-                <img src={productDetail.thumbnail} alt={productDetail.title} />
-                <h1>{productDetail.title}</h1>
-                <h1>{productDetail.attributes}</h1>
-                <h2>{productDetail.original_price}{productDetail.currency_id}</h2>
-                <h2>{productDetail.price}{productDetail.currency_id}</h2>
-                <h2>{productDetail.available_quantity}</h2>
-                <h2>{productDetail.sold_quantity}</h2>
-                <h2>{productDetail.official_store_name}</h2>
-                <h2>{productDetail.shipping}</h2>
-                <h2>{productDetail.country}</h2>
-                </>
-            ) : (
-                <h2>Loading... </h2> // MODIFICAR ESTO OBVIAMENTE
-            )
-           }
+            {
+                productDetail?.title ? (
+                    <>
+                        <img src={productDetail.thumbnail} alt={productDetail.title} />
+                        <h1>{productDetail.title}</h1>
+                        <br />
+                        {renderedAttributes.map((attribute, index) => (
+                            <h2 key={index}>{attribute}</h2>
+                        ))}
+                        <br />
+                        <h2>Precio Orginal: {productDetail.original_price} {productDetail.currency_id}</h2>
+                        <h2>Precio en Sale: {productDetail.price}{productDetail.currency_id}</h2>
+                        <h2>Cantidad disponible: {productDetail.available_quantity} unidades</h2>
+                        <h2>Ya vendidas: {productDetail.sold_quantity} unidades</h2>
+                        <h2>Store Oficial: {productDetail.official_store_name}</h2>
+                        <h2>{productDetail.shipping}</h2>
+                        <h2>Pais: {productDetail.country}</h2>
+                    </>
+                ) : (
+                    <h2>Loading... </h2> // MODIFICAR ESTO OBVIAMENTE
+                )
+            }
         </div>
     )
 }
