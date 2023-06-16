679ffaca9c1c994c2b473915b0fe35c8d435966f
demian <demiancmusic@gmail.com>
2023-06-16 18:15:57 -0300

    about

--- Client/src/pages/About.jsx
+++ Client/src/pages/About.jsx
@@ -1,1 +1,43 @@
-//será el about
+
+import Providers from "@/redux/provider/Provider"
+import style from "./styles/About/About.module.css"
+
+
+const About = () => {
+    return (
+        <div classname={style.conteiner1}>
+            <h2>"Nuestro proyecto es una innovadora plataforma de comercio 
+                electrónico diseñada específicamente para el dinámico y vibrante 
+                mercado de América Latina. Esta plataforma sirve como un mercado 
+                virtual donde los administradores supervisan las operaciones comerciales,
+                mientras que los usuarios interactúan con una amplia gama de productos,
+                reseñas y calificaciones. Los usuarios tienen la libertad de explorar
+                un extenso catálogo de productos y tomar decisiones de compra informadas.
+                Pueden agregar fácilmente los artículos deseados a su carrito de compras,
+                asegurando una experiencia de compra fluida y conveniente.
+                Para aumentar la satisfacción del cliente, la plataforma ofrece códigos
+                de descuento exclusivos para productos seleccionados, lo que permite a
+                los usuarios disfrutar de importantes ahorros en sus compras. Además,
+                nuestro sistema incorpora filtros intuitivos por categoría, que permiten
+                a los usuarios navegar sin esfuerzo a través de una amplia oferta de productos
+                adaptados a sus preferencias. Estos filtros les permiten a los usuarios refinar
+                sus búsquedas según categorías específicas, asegurándose de encontrar exactamente
+                lo que están buscando.
+                Con un enfoque especial en la localización, nuestra plataforma está diseñada para
+                satisfacer las preferencias y necesidades únicas de los diferentes países dentro de
+                la región de América Latina. Al ofrecer filtros específicos por país, empoderamos a
+                los usuarios para descubrir productos que están disponibles específicamente en sus 
+                respectivos países, asegurando una experiencia de compra más personalizada y localizada.
+                En resumen, nuestra plataforma de comercio electrónico para América Latina combina de manera
+                fluida los roles de los administradores, los usuarios y una amplia gama de características como
+                reseñas de productos, calificaciones, códigos de descuento y opciones de filtrado exhaustivas.
+                Al proporcionar un entorno amigable para el usuario y específico para la región, nuestro objetivo
+                es revolucionar el panorama del comercio electrónico en América Latina y redefinir la forma en que
+                las personas interactúan con las compras en línea."</h2>
+        </div>
+    )
+}
+
+
+
+export default About;
