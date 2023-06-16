e2e3db22427f83d3ade1f37476c2e0550fca1014
Victoria-Barrientos <viictoriabarrientos@gmail.com>
2023-06-16 12:46:14 -0300

    Estilos de filtros

--- Client/src/pages/Components/Filter.jsx
+++ Client/src/pages/Components/Filter.jsx
@@ -1,72 +1,43 @@
-//import { axiosAllProducts } from "../../redux/slice/productSlice";
+// import { axiosAllProducts } from "../../redux/slice/productSlice";
+import React, { useState } from 'react';
 import { useDispatch } from "react-redux";
-import CountryImages from "./CountryImages";
-import "./Styles/Filter.module.css"
+import Styles from "./Styles/Filter.module.css"
 
 const Filter = ({ setOrden, setCurrentPage }) => {
     const dispatch = useDispatch();
+    const [selectedCountry, setSelectedCountry] = useState('Argentina');
 
 //   function handleFilterByCountry(event) {
 //     dispatch(axiosAllProducts(event.target.value));
 //   }
 
-//   function handleSort(e) {
-//     e.preventDefault();
-//     dispatch(orderByName(e.target.value));
-//     setCurrentPage(1);
-//     setOrden(`Ordenado ${e.target.value}`);
-//   }
+  function handleSort(event) {
+    setCurrentPage(1);
+    setOrden(`Ordenado ${event.target.value}`);
+    dispatch(orderByName(event.target.value));
+  }
 
-//   function handlePrice(e) {
-//     e.preventDefault();
-//     dispatch(orderByPrice(e.target.value));
-//     setCurrentPage(1);
-//     setOrden(`Ordenado ${e.target.value}`);
-//   }
+  function handlePriceChange(event) {
+    setCurrentPage(1);
+    setOrden(`Ordenado ${event.target.value}`);
+    // dispatch(orderByPrice(event.target.value));
+  }
+
 
   return (
     <div className="filter-container">
-    <div className="bar-promo">
-    <h4>
-    blablablablaasdasddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-    </h4>
-    <button className="btn-promo" >botón para comprar</button>
-    <span>Texto cualquiera</span>
-    </div>
 
+      <label>Precio</label>      
+      <select className={Styles.select} onChange={handlePriceChange}>
+          <option value="menormayor">De menor a mayor</option>
+          <option value="mayormenor">De mayor a menor</option>
+      </select>
 
-      <div className="filter-item-img">
-        <div className="country-images">
-          {Object.entries(CountryImages).map(([country, image]) => (
-            <div key={country} className="country-image"> 
-              <img src={image} alt={country} />
-              <span>{country}</span>
-            </div>
-          ))}
-        </div>
-      </div>
-
-
-
-      <div className="filter-row">
-        <div className="filter-item 1">
-          <label html="price-select">Price:</label>
-          <select id="price-select" className="filter-select" onChange={(e) => handlePrice(e)}>
-            <option value="menormayor">From smallest to Largest</option>
-            <option value="mayormenor">From older to Younger</option>
-          </select>
-        </div>
-        
-        <div className="filter-item tres">
-          <label html="sort-select">Order:</label>
-          <select id="sort-select" className="filter-select" onChange={(e) => handleSort(e)}>
-            <option value="asc">Upward (A-Z)</option>
-            <option value="des">Falling (Z-A)</option>
-          </select>
-        </div>
-      </div>
-
-
+      <label>Orden</label>      
+      <select className={Styles.select} onChange={handleSort}>
+          <option value="asc">A-Z</option>
+          <option value="des">Z-A</option>
+      </select>
 
     </div>
   );
