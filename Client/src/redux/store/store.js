e325fead46f60c0217a2187af7fff39911d0d9d3
demian <demiancmusic@gmail.com>
2023-06-21 13:39:09 -0300

    rating review

--- Client/src/redux/store/store.js
+++ Client/src/redux/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { slice } from "../slice/slice";
 import { productSlice } from "../slice/productSlice";
 import thunk from "redux-thunk";
+import {ratingReviewSlice} from "../slice/ratingReviewSlice"
 
 export default configureStore({
     reducer: {
         names: slice.reducer,
-        products: productSlice.reducer
+        products: productSlice.reducer,
+        reviews: ratingReviewSlice.reducer
     },
     middleware: [thunk]
 })
