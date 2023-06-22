f2895fafb5b0877be4052d6b6b0adbb5c749a465
CristianR1992 <rodriguez.cristianezequiel@hotmail.com>
2023-06-22 21:44:10 +0100

    prueba de traer cada review

--- Client/src/pages/Components/ReviewRating.jsx
+++ Client/src/pages/Components/ReviewRating.jsx
@@ -1,23 +1,33 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getAllReviews } from '../../redux/slice/ratingReviewSlice';
+import { getAllReviewsForProduct } from '../../redux/slice/ratingReviewSlice';
+import { useRouter } from "next/router";
 
 const ReviewRating = () => {
   const dispatch = useDispatch();
   const reviews = useSelector((state) => state.reviews.reviews);
+  const router = useRouter();
+  const { id } = router.query;
+  const productId= id
 
   useEffect(() => {
-    dispatch(getAllReviews());
-  }, [dispatch]);
+    dispatch(getAllReviewsForProduct(productId));
 
+  }, [dispatch, productId]);
+  const suma = reviews.reduce((total, review) => total + review.rating, 0);
+ const promedio = suma / reviews.length
+  
   return (
     <div>
-      <h1>Reviews</h1>
+      <h1>Todas las reviews: </h1>
+      <h2>Valoración promedio: {promedio}</h2>
       <div>
         {/* Renderiza los datos de las reviews */}
         {reviews.map((review) => (
           <div key={review.id}>
-            <p>Rating: {review.rating}</p>
+            <h3>Puntaje: {review.rating}</h3>
+            <p>Descripcion: {review.review_description}</p>
+          
             {/* Renderiza otros detalles de la review si los tienes */}
           </div>
         ))}
--- Server/Handlers/post/postReview.js
+++ Server/Handlers/post/postReview.js
@@ -4,7 +4,7 @@ const postReviewHandler = async (req, res) => {
   const { rating, review_description, productId } = req.body;
   try {
     const createdReview = await postReview( rating, review_description, productId );
-   console.log(createdReview)
+   
     res.status(201).json(createdReview);
   } catch (error) {
     res.status(400).json({ error: error.message });
