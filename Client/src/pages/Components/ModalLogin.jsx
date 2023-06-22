91b72d3530853c2a78347f5bd86ed6e2ee92cc56
unknown <fede.mpz@gmail.com>
2023-06-22 08:09:35 -0300

    Login con funcionalidad completa, sin CSS, falta modularizar y validacion de token

--- Client/src/pages/Components/ModalLogin.jsx
+++ Client/src/pages/Components/ModalLogin.jsx
@@ -3,13 +3,53 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import style from "./Styles/Footerlanding.module.css"
+import axios from 'axios'
 
 function Example() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
 
+  const login = async (form) => {
+    try{ 
+      const response = await axios.post('http://localhost:8000/users/login', form)
+      console.log(response.data);
+        
+    }catch(error){
+        console.log(error.message);
+    }
+  }
+  const handleChange = (event) =>{
+    setForm((prevForm) => ({
+        ...prevForm,
+        [event.target.name]: event.target.value
+      }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+      try {
+        login(form)
+        setForm({
+            email: '',
+            password: '',
+        });
+        // return response
+      } catch (error) {
+
+        setSuccess('Error al crear el usuario'+ error.message);
+        // return error
+      }
+
+
+      
+    
+  };
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
@@ -23,18 +63,28 @@ function Example() {
         <Modal.Body>
           <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="name@example.com"
-                autoFocus
+
+              <label htmlFor="email">
+              E-Mail
+              <input
+                type="text"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                placeholder='Ej: ashketchum@pokemon.com'
               />
-              <Form.Label>PassWord</Form.Label>
-              <Form.Control
+            </label>
+
+              <label htmlFor="password">
+              Contraseña
+              <input
                 type="password"
-                placeholder="insert your password"
-                autoFocus
+                name="password"
+                value={form.password}
+                onChange={handleChange}
+                placeholder='Contraseña'
               />
+            </label>
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -43,7 +93,7 @@ function Example() {
             Close
           </Button>
 
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={handleSubmit}>
             Log In
           </Button>
 
