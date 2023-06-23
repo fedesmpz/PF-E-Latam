7e35635f5db19873b48bd8be06ddbc6926cb207c
Victoria-Barrientos <viictoriabarrientos@gmail.com>
2023-06-23 17:48:47 -0300

    Imported cloudinary and upload method

--- Server/Handlers/put/updateProduct.js
+++ Server/Handlers/put/updateProduct.js
@@ -1,20 +1,21 @@
-const { editProduct } = require("../../Controllers/put/updateProduct")
+const { editProduct } = require("../../Controllers/put/updateProduct");
+const { cloudinary } = require("../../cloudinaryConfig");
 
 const editProductHandler = async(req, res) => {
     try {
         let editedProduct;
         const { id } = req.params;
         const {title, uri, original_price, currency_id, price, sale_price, available_quantity, sold_quantity, official_store_name, shipping, attributes, catalog_listing, promotions, categories, country} = req.body;
-        console.log(uri)
-        // if(uri) {
-        //     const uploadResponse = await cloudinary.uploader.upload(uri, {
-        //         upload_preset: "products-thumbnails"
-        // })
-        //     if(uploadResponse) {
-        //         editedProduct = await editProduct(id, title, uri, original_price, currency_id, price, sale_price, available_quantity, sold_quantity, official_store_name, shipping, attributes, catalog_listing, promotions, categories, country);
-        //     }
-        // }
-        editedProduct = await editProduct(id, title, uri, original_price, currency_id, price, sale_price, available_quantity, sold_quantity, official_store_name, shipping, attributes, catalog_listing, promotions, categories, country);
+        if(uri) {
+            const uploadResponse = await cloudinary.uploader.upload(uri, {
+                upload_preset: "products-thumbnails"
+        })
+            if(uploadResponse) {
+                const thumbnail = uploadResponse.url
+                editedProduct = await editProduct(id, title, thumbnail, original_price, currency_id, price, sale_price, available_quantity, sold_quantity, official_store_name, shipping, attributes, catalog_listing, promotions, categories, country);
+            }
+        }
+        // editedProduct = await editProduct(id, title, uri, original_price, currency_id, price, sale_price, available_quantity, sold_quantity, official_store_name, shipping, attributes, catalog_listing, promotions, categories, country);
         return res.status(200).json(editedProduct)
     } catch(error) {
         console.log(error)
