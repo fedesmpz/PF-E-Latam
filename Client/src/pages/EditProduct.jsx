02e1b3fd0fda0f3171a3f94dac70497d80b24a96
Victoria-Barrientos <viictoriabarrientos@gmail.com>
2023-06-23 16:37:35 -0300

    Added submit handler and useEffects

--- Client/src/pages/EditProduct.jsx
+++ Client/src/pages/EditProduct.jsx
@@ -1,11 +1,11 @@
 'use client'
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { ProductByIdForEditForm, editProduct } from "../redux/slice/productSlice";
+import { ProductByIdForEditForm, editProduct, cleanEditDetail, setEditProductMessage, setEditedProduct } from "../redux/slice/productSlice";
 import { useRouter } from "next/router";
 import Providers from "@/redux/provider/Provider";
 import Link from "next/link";
-import validation from "../utils/formValidation"
+import validation from "../utils/formValidation";
 import NavBar from "./Components/NavBar"
 import SubFooter from "./Components/SubFooter";
 import style from "./Styles/EditProduct/EditProduct.module.css"
@@ -14,65 +14,100 @@ const EditProduct = () => {
     const router = useRouter();
     const { id, countryId, categories } = router.query;
     const dispatch = useDispatch();
-    const [isFormValid, setIsFormValid] = useState(false)
-    let [errors, setErrors] = useState({})
-    const message = useSelector(state => state.products.newProductMessage)
-    const [productThumbnail, setProductThumbnail] = useState("")
+    const [isFormValid, setIsFormValid] = useState(false);
+    let [errors, setErrors] = useState({});
+    const message = useSelector(state => state.products.editProductMessage);
+    const [productThumbnail, setProductThumbnail] = useState("");
     let editDetail = useSelector((state) => state.products.editDetail);
+    const [localEditDetail, setLocalEditDetail] = useState(editDetail);
+    const [showModal, setShowModal] = useState(false);
+    const [showModalEdited, setShowModalEdited] = useState(false);
 
-    // const [editProduct, setEditedProduct] = useState({
-    //   title: "",
-    //   original_price: 0,
-    //   currency_id: "ARS",
-    //   price: 0,
-    //   sale_price: null,
-    //   sold_quantity: 0,
-    //   available_quantity: 0,
-    //   official_store_name: "",
-    //   shipping: null,
-    //   attributes: "",
-    //   promotions: [],
-    //   categories: "",
-    //   country: "Argentina",
-    // })
+    const handleCountryChange = (event) => {
+        let value = event.target.value
+        let aux = value
 
-    // setEditedProduct({
-    //   ...editProduct,
-    //   ...editDetail
-    // })
+        aux = (aux === "Argentina") ? "ARS"
+            : (aux === "Mexico") ? "MXN"
+                : (aux === "Colombia") ? "COP"
+                    : aux;
 
-    console.log(editProduct)
-    
-    const handleCountryChange = () => {
-
+        setLocalEditDetail({
+            ...localEditDetail,
+            currency_id: aux,
+            country: value
+        })
     }
 
     const handleChange = (event) => {
       const prop = event.target.name;
       const value = event.target.value
 
-      editDetail = {
-        ...editDetail,
+      setLocalEditDetail({
+        ...localEditDetail,
         [prop]: value
-      }
+      });
+
+      validation(prop, value, errors, setErrors)
 
     }
 
-    const handleProductThumbnailUpload = () => {
-
+    const handleProductThumbnailUpload = (event) => {
+        const prop = event.target.name
+        const file = event.target.files[0];
+        validation(prop, file, errors, setErrors)
+        transformFile(file)
     }
 
-    const transformFile = () => {
-
+    const transformFile = (file) => {
+        const reader = new FileReader()
+        if(file) {
+            reader.readAsDataURL(file)
+            reader.onloadend = () => {
+                setProductThumbnail(reader.result);
+            }
+        } else {
+            setProductThumbnail("")
+        }
     }
 
-    const handleSubmit = () => {
+    const handleCloseModal = () => {
+        dispatch(setEditProductMessage(""));
+        setShowModalEdited(false)
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        dispatch(editProduct(id, {
+            ...localEditDetail,
+            uri: productThumbnail
+        }))
+
+        setShowModalEdited(true)
+
+        if (isFormValid) {
+            setLocalEditDetail({})
+            setErrors({})
+        }
+        // router.push(`/DetailProduct?countryId=${countryId}&categories=${categories}&id=${id}`)
 
     }
 
     useEffect(() => {
-      dispatch(ProductByIdForEditForm( id, countryId, categories))
+      dispatch(ProductByIdForEditForm( id, countryId, categories));
+      return () => dispatch(cleanEditDetail());
     }, [dispatch, id, countryId, categories])
+
+    useEffect(() => {
+        setLocalEditDetail(editDetail);
+        setProductThumbnail(editDetail.thumbnail)
+    }, [editDetail]);
+
+    useEffect(() => {
+        const isValid = (localEditDetail !== editDetail) && Object.values(errors).every((error) => error === "");
+        setIsFormValid(isValid);
+    }, [errors, localEditDetail]);
 
     return (
         <div className={style.container}>
@@ -86,7 +121,7 @@ const EditProduct = () => {
                         </svg>
                     </button>
                 </Link>
-                <div>
+                {/* <div>
                 {message && (
                     <div className={style.successMessageContainer}>
                     <p className={style.successMessage}>{message}<button onClick={handleCloseMessage} className={style.closeButton}>
@@ -94,27 +129,27 @@ const EditProduct = () => {
                     </button></p>
                     </div>
                 )}
-                </div>
+                </div> */}
             </div>
             <div className={style.containerForm}>
                 <form encType="multipart/form-data" onSubmit={handleSubmit}>
 
                     <div>
                         <label htmlFor="title" className={style.label}>Titulo del producto</label>
-                        <input type="text" name="title" value={editDetail.title} onChange={handleChange} />
+                        <input type="text" name="title" value={localEditDetail.title} onChange={handleChange} />
 
                         {errors.title && <p>{errors.title}</p>}
                     </div>
 
                     <div>
                         <label htmlFor="official_store_name" className={style.label}>Marca</label>
-                        <input type="text" name="official_store_name" value={editDetail.official_store_name} onChange={handleChange} />
+                        <input type="text" name="official_store_name" value={localEditDetail.official_store_name} onChange={handleChange} />
                         {errors.official_store_name && <p>{errors.official_store_name}</p>}
                     </div>
 
                     <div>
                         <label htmlFor="country" className={style.label}>País</label>
-                        <select onChange={handleCountryChange} name="country" id="country" className={style.selectField}>
+                        <select onChange={handleCountryChange} name="country" id="country" value={localEditDetail.country} className={style.selectField}>
                             <option value="Argentina"> Argentina </option>
                             <option value="Colombia"> Colombia </option>
                             <option value="Mexico"> México </option>
@@ -125,14 +160,14 @@ const EditProduct = () => {
                         <label htmlFor="original_price" className={style.label}>Precio</label>
                         <div className={style.priceCont}>
                             <span className={style.priceTag}>$ {editDetail.currency_id}</span>
-                            <input className={style.priceInput} type="number" name="original_price" value={editDetail.original_price} onChange={handleChange} />
+                            <input className={style.priceInput} type="number" name="original_price" value={localEditDetail.original_price} onChange={handleChange} />
                         </div>
                         {errors.original_price && <p>{errors.original_price}</p>}
                     </div>
 
                     <div>
                         <label htmlFor="available_quantity" className={style.label}>Stock disponible</label>
-                        <input type="number" name="available_quantity" value={editDetail.available_quantity} onChange={handleChange} />
+                        <input type="number" name="available_quantity" value={localEditDetail.available_quantity} onChange={handleChange} />
                         {errors.available_quantity && <p>{errors.available_quantity}</p>}
                     </div>
 
@@ -144,7 +179,7 @@ const EditProduct = () => {
 
                     <div>
                         <label htmlFor="sale_price" className={style.label}>Quiere colocar este producto en oferta?</label>
-                        <select name="sale_price" id="sale_price" value={editDetail.sale_price} onChange={handleChange}>
+                        <select name="sale_price" id="sale_price" value={localEditDetail.sale_price} onChange={handleChange}>
                             <option value="">---</option>
                             <option value={true}>Sí</option>
                             <option value={false}>No</option>
@@ -162,7 +197,7 @@ const EditProduct = () => {
 
                     <div>
                         <label htmlFor="shipping" className={style.label}>Este producto posee envío gratis?</label>
-                        <select name="shipping" id="shipping" value={editDetail.shipping} onChange={handleChange}>
+                        <select name="shipping" id="shipping" value={localEditDetail.shipping} onChange={handleChange}>
                             <option value="">---</option>
                             <option value={true} >Sí</option>
                             <option value={false} >No</option>
@@ -172,7 +207,7 @@ const EditProduct = () => {
 
                     <div>
                         <label htmlFor="categories" className={style.label}>Elija una categoría para el producto</label>
-                        <select onChange={handleChange} name="categories" id="categories" className={style.selectField}>
+                        <select onChange={handleChange} name="categories" id="categories" value={localEditDetail.categories} className={style.selectField}>
                             <option value="">---</option>
                             <option value="computacion"> Computación </option>
                             <option value="celulares"> Celulares </option>
@@ -185,7 +220,7 @@ const EditProduct = () => {
 
                     <div>
                         <label htmlFor="attributes" className={style.label}>Descripción</label>
-                        <textarea type="text-area" name="attributes" value={editDetail.attributes} onChange={handleChange} rows="4" cols="50" />
+                        <textarea type="text-area" name="attributes" value={localEditDetail.attributes} onChange={handleChange} rows="4" cols="50" />
                         {errors.attributes && <p>{errors.attributes}</p>}
                     </div>
 
@@ -194,35 +229,47 @@ const EditProduct = () => {
 
                     </div>
                 </form>
+                <>
+                {message && showModalEdited && (
+                <div className={style.modal}>
+                  <div className={style.modalContent}>
+                    <p>{message}</p>
+                    <div className={style.modalButtons}>
+                      <button onClick={handleCloseModal}>x</button>
+                    </div>
+                  </div>
+                </div>)
+                }
+                </>
                 <div className={style.secondColumn}>
                     <h2 className={style.PreviewofProduct}>PREVISUALIZACION DEL PRODUCTO</h2>
                     <div className={style.firstRow}>
                     <div className={style.thumbnailContainer}>
-                    {!editDetail.thumbnail && 
+                    {!productThumbnail && 
                     <p className={style.previewTitleThumbnail}>La vista previa de la imagen aparecera aqui</p>}
-                    {editDetail.thumbnail && <img src={editDetail.thumbnail} className={style.thumbnail}  alt="product_thumbnail"></img>}
+                    {productThumbnail && <img src={productThumbnail} className={style.thumbnail}  alt="product_thumbnail"></img>}
                     </div>
                     <div>
-                        <h2 className={style.previewValue}>{editDetail.title ? editDetail.title : `Titulo del producto`}</h2>
-                        <h3 className={style.previewValue}>{editDetail.official_store_name ? editDetail.official_store_name : `Marca del producto`}</h3>
-                        <span className={style.previewLines}><h3 className={style.previewLabel}>Pais</h3><h3 className={style.previewValue}>{editDetail.country}</h3></span>
+                        <h2 className={style.previewValue}>{localEditDetail.title ? localEditDetail.title : `Titulo del producto`}</h2>
+                        <h3 className={style.previewValue}>{localEditDetail.official_store_name ? localEditDetail.official_store_name : `Marca del producto`}</h3>
+                        <span className={style.previewLines}><h3 className={style.previewLabel}>Pais</h3><h3 className={style.previewValue}>{localEditDetail.country}</h3></span>
                         <span className={style.previewLines}><h3 className={style.previewLabel}>Precio</h3>
                         <h3 className={style.previewValue}>$ {editDetail.currency_id} {editDetail.original_price}</h3></span>
-                        <span className={style.previewLines}><h3 className={style.previewLabel}>Stock disponible</h3><h3 className={style.previewValue}> {editDetail.available_quantity}</h3></span>
+                        <span className={style.previewLines}><h3 className={style.previewLabel}>Stock disponible</h3><h3 className={style.previewValue}> {localEditDetail.available_quantity}</h3></span>
                     </div>
                     </div>
 
                     <div className={style.previewData}>
                         <span className={style.previewLines}><h3 className={style.previewLabel}>Producto en oferta?</h3>
-                        <p className={style.previewValue}>{editDetail.sale_price ? editDetail.sale_price : `-`}</p></span>
+                        <p className={style.previewValue}>{localEditDetail.sale_price ? localEditDetail.sale_price : `-`}</p></span>
                         <span className={style.previewLines}> <h3 className={style.previewLabel}>Precio de oferta</h3>
-                        <p className={style.previewValue}>{editDetail.price}</p></span>
+                        <p className={style.previewValue}>{localEditDetail.price}</p></span>
                         <span className={style.previewLines}><h3 className={style.previewLabel}>Envio gratis</h3>
-                        <p className={style.previewValue}>{editDetail.shipping ? editDetail.shipping : `-`}</p></span>
+                        <p className={style.previewValue}>{localEditDetail.shipping ? localEditDetail.shipping : `-`}</p></span>
                         <span className={style.previewLines}><h3 className={style.previewLabel}>Categoria</h3>
-                        <h3 className={style.previewValue}>{editDetail.categories ? editDetail.categories : `-` }</h3></span>  
+                        <h3 className={style.previewValue}>{localEditDetail.categories ? localEditDetail.categories : `-` }</h3></span>  
                         <h3 className={style.previewLabel}>Descripcion</h3>
-                        <p className={style.previewValue}>{editDetail.attributes ? editDetail.attributes : `Describe el producto brindando información clara y detallada sobre el artículo para ayudar a los clientes a comprender sus características, beneficios y especificaciones; tambien intenta incluir su propósito, función y uso. Enumera las características específicas del producto, como tamaño, dimensiones, materiales, color, capacidad, peso, etc. Si el producto tiene características técnicas, como velocidad, capacidad de almacenamiento o conectividad, asegúrate de incluirlas aquí.`}</p>                  
+                        <p className={style.previewValue}>{localEditDetail.attributes ? localEditDetail.attributes : `Describe el producto brindando información clara y detallada sobre el artículo para ayudar a los clientes a comprender sus características, beneficios y especificaciones; tambien intenta incluir su propósito, función y uso. Enumera las características específicas del producto, como tamaño, dimensiones, materiales, color, capacidad, peso, etc. Si el producto tiene características técnicas, como velocidad, capacidad de almacenamiento o conectividad, asegúrate de incluirlas aquí.`}</p>                  
                     </div>
                 </div>
 
