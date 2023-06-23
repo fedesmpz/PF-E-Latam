cacaf0e5c8790a0958451fffcba2578bfa3f7870
Victoria-Barrientos <viictoriabarrientos@gmail.com>
2023-06-23 17:05:40 -0300

    Fixed unique name bug

--- Server/Controllers/put/updateProduct.js
+++ Server/Controllers/put/updateProduct.js
@@ -6,11 +6,7 @@ const editProduct = async(id, title, thumbnail, original_price, currency_id, pri
         if (!product) {
             throw new Error('El producto no existe');
         }
-        const existingProduct = await Product.findOne({ where: { title } });
-        if (existingProduct.id !== product.id) {
-            throw new Error('El título del producto ya está en uso');
-        }
-        if (existingProduct) {
+        if (product) {
             let updatedProduct = await Product.update({ title, thumbnail, original_price, currency_id, price, sale_price, available_quantity, sold_quantity, official_store_name, shipping, attributes, catalog_listing, promotions, categories, country }, { where: { id: id }});
         }
         return `Producto ${id} actualizado correctamente`
