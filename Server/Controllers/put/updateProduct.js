35a5a5df5fed1c2d1b3a633be8eb3a05d40b35e4
Victoria-Barrientos <viictoriabarrientos@gmail.com>
2023-06-23 16:41:15 -0300

    Fixed returning message bug

--- Server/Controllers/put/updateProduct.js
+++ Server/Controllers/put/updateProduct.js
@@ -1,6 +1,6 @@
 const { Product }= require("../../db.js")
 
-const editProduct = async(id, title, thumbnail, original_price, currency_id, price, sale_price, available_quantity, sold_quantity, official_store_name, shipping, attributes, catalog_listing, discounts, promotions, categories, country) => {
+const editProduct = async(id, title, thumbnail, original_price, currency_id, price, sale_price, available_quantity, sold_quantity, official_store_name, shipping, attributes, catalog_listing, promotions, categories, country) => {
     try {
         const product = await Product.findByPk(id)
         if (!product) {
@@ -10,8 +10,10 @@ const editProduct = async(id, title, thumbnail, original_price, currency_id, pri
         if (existingProduct.id !== product.id) {
             throw new Error('El título del producto ya está en uso');
         }
-        const updatedProduct = await Product.update({ title, thumbnail, original_price, currency_id, price, sale_price, available_quantity, sold_quantity, official_store_name, shipping, attributes, catalog_listing, discounts, promotions, categories, country }, { where: { id: id }});
-        return "Producto actualizado correctamente"
+        if (existingProduct) {
+            let updatedProduct = await Product.update({ title, thumbnail, original_price, currency_id, price, sale_price, available_quantity, sold_quantity, official_store_name, shipping, attributes, catalog_listing, promotions, categories, country }, { where: { id: id }});
+        }
+        return `Producto ${id} actualizado correctamente`
     } catch(error) {
         throw error
     }
