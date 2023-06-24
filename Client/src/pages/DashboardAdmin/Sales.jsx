175e583e3dffa05457cbc422d2813b30f99d57ea
camilobit <camiloacevedovargas@gmail.com>
2023-06-24 18:36:53 -0500

    ventas del admin

--- Client/src/pages/DashboardAdmin/Sales.jsx
+++ Client/src/pages/DashboardAdmin/Sales.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Sidebar from '../ComponentsAdmin/SideBar/SideBar'
 import AllOrders from '../ComponentsAdmin/Orders/AllOrders'
+import StylesAdmin from "../styles/DashboardAdmin/DashboardAdmin.module.css"
+import { useState } from 'react'
 
 const Sales = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
+    <div className={StylesAdmin.containerAdmin} >
         <Sidebar />
+    <div className={StylesAdmin.containerHomeAdmin}style={{ marginLeft: isOpen ? '120px' : (!isOpen && '60px') }}>
+        <div className={StylesAdmin.containerSuperior}>
+        <h1></h1>
+        </div>
         <AllOrders />
+    </div>
     </div>
   )
 }
