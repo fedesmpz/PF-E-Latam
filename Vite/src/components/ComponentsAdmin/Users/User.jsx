a671cb9955e77adf1b4f9339bcb46bcd5cca597f
camilobit <camiloacevedovargas@gmail.com>
2023-06-28 22:36:09 -0500

    adicion de productos mas vendidos, falta css y filtros gracias viole jajaj

--- Vite/src/components/ComponentsAdmin/Users/User.jsx
+++ Vite/src/components/ComponentsAdmin/Users/User.jsx
@@ -1,13 +1,7 @@
-import React, { useState } from "react";
 import Styles from "../Products/StylesProducts/AllProductsAdmin.module.css";
 import { BiArchive } from "react-icons/bi";
-import { GrUserAdmin } from "react-icons/gr";
-import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
-import UserDetails from "./UserDetails";
-import { useDispatch } from "react-redux";
-import { getUserById } from "../../../redux/slice/userSlice";
-
+import { GrUserAdmin } from "react-icons/gr"
+import UserDetailsModal from "./UserDetailsModal";
 const User = ({
   id,
   name,
@@ -21,7 +15,7 @@ const User = ({
   postal_code,
   createdAt,
 }) => {
-  let isAdmin;
+    let isAdmin;
   if (admin === false) {
     isAdmin = "No";
   } else if (admin === "true") {
@@ -38,73 +32,52 @@ const User = ({
     }
     return name;
   };
-
-  const dispatch = useDispatch();
-
-  const handleOpenModal = () => {
-    setShowModal(true);
-    dispatch(getUserById(id));
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
-  const [showModal, setShowModal] = useState(false);
-
   return (
-    <>
-      <li key={id} className={Styles.order}>
-        <div className={Styles.faShop}>
-          <img
-            src={profile_picture}
-            alt="Producto"
-            style={{
-              width: "auto",
-              height: "3rem",
-            }}
-          />
-        </div>
-        <div className={Styles.textContainer}>
-          <p className="text-gray-800 font-bold">{shortenname(name)}</p>
-          <p className="text-gray-400 text-sm">Pais: {country}</p>
-          <p className="text-gray-400 text-sm">Email: {email}</p>
-        </div>
-        <p className={Styles.date}>
-          <p className={Styles["text"]}>
-            {}
-            <br></br>
-            <span className={Styles["text"]}>
-              <GrUserAdmin className={Styles["icon"]} />
-              <br></br>Admin {isAdmin}
-            </span>
-          </p>
+    <div>
 
-          <button className={Styles["button"]} onClick={handleOpenModal}>
+    <li key={id} className={Styles.order}>
+      <div className={Styles.faShop}>
+        <img
+          src={profile_picture}
+          alt="Producto"
+          style={{
+            width: "auto",
+            height: "3rem",
+          }}
+        />
+      </div>
+      <div className={Styles.textContainer}>
+        <p className="text-gray-800 font-bold">{shortenname(name)}</p>
+        <p className="text-gray-400 text-sm">Pais: {country}</p>
+        <p className="text-gray-400 text-sm">Email: {email}</p>
+      </div>
+      <p className={Styles.date}>
+        <p className={Styles["text"]}>
+          {}
+          <br></br>
+          <span className={Styles["text"]}>
+            <GrUserAdmin className={Styles["icon"]} />
+            <br></br>Admin {isAdmin}
+          </span>
+        </p>
+
+<UserDetailsModal/>
+        {/* <a
+          className={Styles["a"]}
+          href={``}
+        >
+          <p className={Styles["text"]}>
+            <br></br>
             <BiArchive className={Styles["icon"]} />
             <span className={Styles["text"]}>
               <br></br>Detalles
             </span>
-          </button>
-        </p>
-      </li>
-
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>User Details</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {showModal && <UserDetails id={id} />}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+          </p>
+        </a> */}
+      </p>
+    </li>
+    </div>
   );
 };
 
 export default User;
-
--- Vite/src/components/DashboardAdmin/Statistics.jsx
+++ Vite/src/components/DashboardAdmin/Statistics.jsx
@@ -4,16 +4,24 @@ import SalesCard from '../ComponentsAdmin/Statistics/SalesCard'
 import SalesStats from '../ComponentsAdmin/Statistics/SalesStats'
 import LineChart from '../ComponentsAdmin/Statistics/LineChart'
 import StylesAdmin from "./DashboardAdmin.module.css";
+import { useState } from 'react'
+import NavbarAdmin from '../NavBarAdmin/NavbarAdmin'
 
 const Statistics = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
   const totalSales = 100;
   const todaySales = 12;
   const monthSales = 71;
 
   return (
-    <div className={StylesAdmin.container}>
+    <div className={StylesAdmin.containerAdmin} >
       <Sidebar />
+      <div className={StylesAdmin.containerHomeAdmin} style={{ marginLeft: isOpen ? '120px' : (!isOpen && '60px') }}>
+      <NavbarAdmin />
+        <div className={StylesAdmin.containerSuperior}>
+        </div>
+        <div className={StylesAdmin.containerProducts}>
       <SalesCard 
       totalSales={totalSales}
       todaySales={todaySales}
@@ -21,6 +29,10 @@ const Statistics = () => {
       />
       <SalesStats />
       <LineChart />
+          
+        </div>
+        
+      </div>
     </div>
   )
 }
