55c0c73d981221591738ed490b2581fc6866caab
Victoria-Barrientos <viictoriabarrientos@gmail.com>
2023-06-29 12:43:20 -0300

    Adapted back to handle total_price

--- Server/Handlers/post/addProductsToCart.js
+++ Server/Handlers/post/addProductsToCart.js
@@ -3,12 +3,8 @@ const { addProductToCart } = require("../../Controllers/post/addProductToCart")
 const addProductsToCartHandler = async(req, res) => {
     try {
         const { cartId } = req.params;
-        let products = req.body;
-        let cart;
-        for (const product of products) {
-            const { id, quantity } = product;
-            cart = await addProductToCart(cartId, id, quantity);
-        }
+        let {products, total_price} = req.body;
+        const cart = await addProductToCart(cartId, products, total_price);
         res.status(200).json(cart)
     } catch(error) {
         console.log(error);
@@ -19,5 +15,27 @@ const addProductsToCartHandler = async(req, res) => {
 module.exports = {
     addProductsToCartHandler
 }
+
+// const { addProductToCart } = require("../../Controllers/post/addProductToCart")
+
+// const addProductsToCartHandler = async(req, res) => {
+//     try {
+//         const { cartId } = req.params;
+//         let products = req.body;
+//         let cart;
+//         for (const product of products) {
+//             const { id, quantity } = product;
+//             cart = await addProductToCart(cartId, id, quantity);
+//         }
+//         res.status(200).json(cart)
+//     } catch(error) {
+//         console.log(error);
+//         res.status(400).json({ error: error.message })
+//     }
+// }
+
+// module.exports = {
+//     addProductsToCartHandler
+// }
 
 
