336c5985c629c4228930d415d70514ab15c9d24e
Violeta <holasoyviole@gmail.com>
2023-06-30 17:52:48 -0300

    eliminaciones

---
+++ Vite/src/components/ComponentsAdmin/Statistics/modelos..js
@@ -0,0 +1,62 @@
+export const examples = [
+  {
+    id: "1",
+    total_price: 99.99,
+    user_id: "user1",
+    products_id: ["product1", "product2", "product3"],
+  },
+  {
+    id: "2",
+    total_price: 149.99,
+    user_id: "user2",
+    products_id: ["product4", "product5"],
+  },
+  {
+    id: "3",
+    total_price: 79.99,
+    user_id: "user3",
+    products_id: ["product2", "product3"],
+  },
+  {
+    id: "4",
+    total_price: 199.99,
+    user_id: "user1",
+    products_id: ["product1", "product5"],
+  },
+  {
+    id: "5",
+    total_price: 129.99,
+    user_id: "user4",
+    products_id: ["product3", "product4"],
+  },
+  {
+    id: "6",
+    total_price: 69.99,
+    user_id: "user2",
+    products_id: ["product2"],
+  },
+  {
+    id: "7",
+    total_price: 299.99,
+    user_id: "user3",
+    products_id: ["product1", "product3", "product4"],
+  },
+  {
+    id: "8",
+    total_price: 89.99,
+    user_id: "user4",
+    products_id: ["product5"],
+  },
+  {
+    id: "9",
+    total_price: 179.99,
+    user_id: "user1",
+    products_id: ["product2", "product3", "product4"],
+  },
+  {
+    id: "10",
+    total_price: 119.99,
+    user_id: "user2",
+    products_id: ["product1", "product5"],
+  },
+];
