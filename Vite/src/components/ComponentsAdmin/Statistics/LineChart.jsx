348732af0c213c9d7003a37315817d8a89fff08f
Violeta <holasoyviole@gmail.com>
2023-06-30 17:45:29 -0300

    cambio en la LineChart

--- Vite/src/components/ComponentsAdmin/Statistics/LineChart.jsx
+++ Vite/src/components/ComponentsAdmin/Statistics/LineChart.jsx
@@ -23,6 +23,14 @@ Chart.register(
 );
 
 export const options = {
+  scales: {
+    y: {
+      min: 0
+    },
+    x: {
+      ticks: { color: 'rgba(255, 99, 132)'}
+    }
+  },
   responsive: true,
   plugins: {
     legend: {
@@ -42,25 +50,25 @@ export const data = {
   datasets: [
     {
       label: 'Celulares',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 10000 })),
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
     },
     {
       label: 'Computacion',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 10000 })),
       borderColor: 'rgb(53, 162, 235)',
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
     },
     {
       label: 'Electronica',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 10000 })),
       borderColor: 'rgb(90, 235, 53)',
       backgroundColor: 'rgba(90, 235, 53, 0.5)',
     },
     {
       label: 'Videojuegos',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 10000 })),
       borderColor: 'rgb(196, 53, 235)',
       backgroundColor: 'rgba(196, 53, 235, 0.5)',
     },
