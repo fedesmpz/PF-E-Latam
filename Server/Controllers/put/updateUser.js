073b1f5644e2ec2bc00d688e6eebaf9c353c2a20
fedesmpz <71994310+fedesmpz@users.noreply.github.com>
2023-07-02 08:50:40 -0300

    Update updateUser.js

--- Server/Controllers/put/updateUser.js
+++ Server/Controllers/put/updateUser.js
@@ -3,8 +3,9 @@ const {Cart}=require("../../db");
 
 const { currencyIdValidator } = require("../../Utilities/currencyIdValidator.js")
 
-const updateUser = async(id,name,surname,email,birth_date,profile_picture,country,city,address,postal_code)=>{
+const updateUser = async(userData)=>{
 
+const {id,name,surname,email,birth_date,profile_picture,country,city,address,postal_code, admin, superAdmin} = userData
 try {
     const user = await User.findByPk(id, {
         include: Cart
@@ -17,7 +18,7 @@ try {
         currency_id = currencyIdValidator(country)
     };
     const updatedUser = await User.update({
-            name,surname,email,birth_date,profile_picture,country,city,address,postal_code
+            name,surname,email,birth_date,profile_picture,country,city,address,postal_code, admin, superAdmin
         },
         {where:{id:id}}
     );
@@ -29,7 +30,28 @@ try {
           { where: { userId: id } }
         );
     }
-    return "Usuario actulizado correctamente"    
+    const userUpdated = await User.findOne({ where: { id: id }});
+    const cartUpdated = await Cart.findOne({ where: { userId: id } });
+
+    const userReturned = {
+        userId: userUpdated.id,
+        name: userUpdated.name,
+        surname: userUpdated.surname,
+        email: userUpdated.email,
+        access: true,
+        verified: userData.verified,
+        isAdmin: userUpdated.isAdmin, 
+        isSuperAdmin: userUpdated.isSuperAdmin,
+        postal_code: userUpdated.postal_code,
+        address: userUpdated.address,
+        city: userUpdated.city,
+        country: userUpdated.country,
+        cartId: cartUpdated.id,
+        firebaseId: userUpdated.firebaseId
+    };
+
+
+    return userReturned
 } catch (error) {
     throw new Error('Error al actualizar el usuario');
 }
