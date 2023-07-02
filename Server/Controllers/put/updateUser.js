6cba8c139ffe6792fd93e7a6731e1fd816298a5f
Victoria-Barrientos <viictoriabarrientos@gmail.com>
2023-07-02 13:58:02 -0300

    Fixed handler and controller again

--- Server/Controllers/put/updateUser.js
+++ Server/Controllers/put/updateUser.js
@@ -1,12 +1,82 @@
-const {User}=require("../../db");
-const {Cart}=require("../../db");
+// const {User}=require("../../db");
+// const {Cart}=require("../../db");
 
+// const { currencyIdValidator } = require("../../Utilities/currencyIdValidator.js")
+
+
+// const updateUser = async(userData)=>{
+
+// const {id,name,surname,email,birth_date,profile_picture,country,city,address,postal_code, admin, superAdmin} = userData
+
+// try {
+//     const user = await User.findByPk(id, {
+//         include: Cart
+//     })
+//     if(!user){
+//         throw new Error("No se encontro usuario con ese ID para su actualizacion")
+//     }
+//     let currency_id = null
+//     if(user.country !== country) {
+//         currency_id = currencyIdValidator(country)
+//     };
+//     const updatedUser = await User.update({
+//             name,surname,email,birth_date,profile_picture,country,city,address,postal_code, admin, superAdmin
+//         },
+//         {where:{id:id}}
+//     );
+//     if (currency_id) {
+//         const updatedCart = await Cart.update(
+//           {
+//             currency_id: currency_id
+//           },
+//           { where: { userId: id } }
+//         );
+//     }
+
+//     let userReturned = {}
+//    if(updatedUser) {
+//     const userUpdated = await User.findOne({ where: { id: id }});
+//     const cartUpdated = await Cart.findOne({ where: { userId: id } });
+
+//     userReturned = {
+//         userId: userUpdated.id,
+//         name: userUpdated.name,
+//         surname: userUpdated.surname,
+//         email: userUpdated.email,
+//         access: true,
+//         verified: userData.verified,
+//         isAdmin: userUpdated.isAdmin, 
+//         isSuperAdmin: userUpdated.isSuperAdmin,
+//         postal_code: userUpdated.postal_code,
+//         address: userUpdated.address,
+//         city: userUpdated.city,
+//         country: userUpdated.country,
+//         cartId: cartUpdated.id,
+//         firebaseId: userUpdated.firebaseId
+//     };
+//     } else {
+//       userReturned = userData
+//     }
+
+
+//     return userReturned
+
+// } catch (error) {
+//     throw new Error('Error al actualizar el usuario');
+// }
+
+// }
+
+// module.exports={
+//     updateUser
+// }
+
+const {User, Cart}=require("../../db");
 const { currencyIdValidator } = require("../../Utilities/currencyIdValidator.js")
 
+const updateUser = async(id, userData)=>{
 
-const updateUser = async(userData)=>{
-
-const {id,name,surname,email,birth_date,profile_picture,country,city,address,postal_code, admin, superAdmin} = userData
+const {name,surname,email,birth_date,profile_picture,country,city,address,postal_code, admin, superAdmin} = userData
 
 try {
     const user = await User.findByPk(id, {
@@ -32,39 +102,18 @@ try {
           { where: { userId: id } }
         );
     }
-
-  let userReturned = {}
-   if(updatedUser) {
-    const userUpdated = await User.findOne({ where: { id: id }});
-    const cartUpdated = await Cart.findOne({ where: { userId: id } });
-
-    userReturned = {
-        userId: userUpdated.id,
-        name: userUpdated.name,
-        surname: userUpdated.surname,
-        email: userUpdated.email,
-        access: true,
-        verified: userData.verified,
-        isAdmin: userUpdated.isAdmin, 
-        isSuperAdmin: userUpdated.isSuperAdmin,
-        postal_code: userUpdated.postal_code,
-        address: userUpdated.address,
-        city: userUpdated.city,
-        country: userUpdated.country,
-        cartId: cartUpdated.id,
-        firebaseId: userUpdated.firebaseId
-    };
-    } else {
-      userReturned = userData
+    let userUpdated;
+    if(updatedUser) {
+        userUpdated = await User.findByPk(id, {
+            include: Cart
+        })
     }
-
-
-    return userReturned
+    return userUpdated
 
 } catch (error) {
-    throw new Error('Error al actualizar el usuario');
+    throw error;
 }
-
+ 
 }
 
 module.exports={
--- Server/Handlers/put/updateUser.js
+++ Server/Handlers/put/updateUser.js
@@ -1,12 +1,29 @@
+// const {updateUser}= require("../../Controllers/put/updateUser")
+
+// const updateUserHandler= async(req,res)=>{
+//     const {id} = req.params 
+//     const userData = req.body
+//     try {
+
+//         const update = await updateUser( userData )
+
+//         return res.status(200).json(update)
+//     } catch (error) {
+//          res.status(400).json({ error: error.message });
+//     }
+// }
+
+// module.exports={
+//     updateUserHandler
+// }
+
 const {updateUser}= require("../../Controllers/put/updateUser")
 
 const updateUserHandler= async(req,res)=>{
     const {id} = req.params 
     const userData = req.body
     try {
-
-        const update = await updateUser( userData )
-
+        const update = await updateUser(id, userData )
         return res.status(200).json(update)
     } catch (error) {
          res.status(400).json({ error: error.message });
