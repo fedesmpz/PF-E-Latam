dc61235993265bef427d371d4beacd863054eb83
Violeta <holasoyviole@gmail.com>
2023-07-02 14:55:51 -0300

    las top cards ya traen el total de los usuarios y la cantidad de admins real, tambien estan en español

--- Vite/src/components/ComponentsAdmin/Statistics/TopCards.jsx
+++ Vite/src/components/ComponentsAdmin/Statistics/TopCards.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 import Styles from "./styles/topCards.module.css";
+import { getUsers } from "../../../redux/slice/userSlice";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-const TopCards = ({ totalUsers, totalAdmins, totalProducts, totalSales }) => {
+
+const TopCards = ({ totalProducts }) => {
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(getUsers());
+  },[])
+
+  const allUsers = useSelector((state) => state.user.users);
+  const totalAdmins = allUsers.filter((user) => user.admin).length;
+
   return (
     <div className={Styles.cardtop}>
-      <h3 className={Styles.coment}>Stats</h3>
+      <h3 className={Styles.coment}>Estadisticas</h3>
       <div className={Styles.division}>
-        <p>Total Users:</p>
-        <span>{totalUsers}</span>
+        <p>Total Usuarios:</p>
+        <span>{allUsers.length}</span>
       </div>
       <div className={Styles.division}>
         <p>Total Admins:</p>
         <span>{totalAdmins}</span>
       </div>
       <div className={Styles.division}>
-        <p>Total Products:</p>
+        <p>Total Productos:</p>
         <span>{totalProducts}</span>
       </div>
     </div>
