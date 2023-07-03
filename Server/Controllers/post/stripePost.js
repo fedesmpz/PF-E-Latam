0c3ba9e7e4abf0664649d7dac163e2ab3409cfc4
fedesmpz <71994310+fedesmpz@users.noreply.github.com>
2023-07-03 12:15:30 -0300

    Update stripePost.js

--- Server/Controllers/post/stripePost.js
+++ Server/Controllers/post/stripePost.js
@@ -1,10 +1,10 @@
 const { Sale } = require("../../db.js")
 
-const stripePost = async (amount,products_id,email) => {
+const stripePost = async (amount,products_id,receipt_email) => {
         const newSale = await Sale.create({
             total_price:amount,
             products_id:products_id,
-            user_id: email,
+            user_id: receipt_email,
         })
     }
 
