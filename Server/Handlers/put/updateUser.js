8096ca018829d3cd4bb6c4e5414b4f15ccfccecf
fedesmpz <71994310+fedesmpz@users.noreply.github.com>
2023-07-03 13:04:35 -0300

    Update updateUser.js

--- Server/Handlers/put/updateUser.js
+++ Server/Handlers/put/updateUser.js
@@ -4,7 +4,7 @@ const updateUserHandler= async(req,res)=>{
     const {id} = req.params 
     const userData = req.body
     try {
-        const update = await updateUser( userData )
+        const update = await updateUser(id, userData )
         return res.status(200).json(update)
     } catch (error) {
          res.status(400).json({ error: error.message });
