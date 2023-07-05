18d71d8a1885a5c69be41ee0254af74930fabcb4
fedesmpz <71994310+fedesmpz@users.noreply.github.com>
2023-07-05 15:56:17 -0300

    Update getReviewById.js

--- Server/Handlers/get/getReviewById.js
+++ Server/Handlers/get/getReviewById.js
@@ -2,9 +2,10 @@
 const {getReviewById} = require("../../Controllers/get/getReviewByid");
 
 const getReviewByIdHandler = async (req, res) => {
-  const { reviewID } = req.params;
+  const { id } = req.query;
+
   try {
-    const review = await getReviewById(reviewID);
+    const review = await getReviewById(id);
     res.status(200).json(review);
   } catch (error) {
     res.status(400).json({ error: error.message });
