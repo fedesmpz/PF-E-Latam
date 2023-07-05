83c34d0f2e4bf50d532b54797e003a46e09a0f9e
fedesmpz <71994310+fedesmpz@users.noreply.github.com>
2023-07-05 07:31:35 -0300

    Create updateStockHandler.js

---
+++ Server/Controllers/put/updateStockHandler.js
@@ -0,0 +1,20 @@
+const { updateStock } = require("../../Controllers/put/updateStock");
+
+
+const updateStockHandler = async(req, res) => {
+    try {
+
+
+      const productUpdated = await updateStock(req.body);
+      return res.status(200).json(productUpdated);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ error: error.message });
+    }
+  };
+  
+  
+
+module.exports = {
+    updateStockHandler
+}
