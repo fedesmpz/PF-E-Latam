14c050f781a4bc5a4bcd25aff8447895d6816e83
fedesmpz <71994310+fedesmpz@users.noreply.github.com>
2023-07-05 11:54:33 -0300

    Update user.js

--- Server/Routes/user.js
+++ Server/Routes/user.js
@@ -4,12 +4,13 @@ const router = Router();
 const { registerHandler } = require("../Handlers/post/register")
 const { getUserByIdHandler } = require("../Handlers/get/getUserById")
 const {deleteUserHandler } = require("../Handlers/delete/deleteUser")
-const { updateUserHandler } = require("../Handlers/put/updateUser")
+const { updateUserHandler, updateUserImageHandler } = require("../Handlers/put/updateUser")
 const { googleLoginHandler, googleExist } = require("../Handlers/post/googleLogin")
 const { loginHandler } = require("../Handlers/post/loginHandler")
 const { getToken, validateToken } = require("../Handlers/post/tokenHandler")
 const { getUserAddressHandler } = require("../Handlers/get/getUserAddress")
 const { getAllUsersHandler } = require("../Handlers/get/getAllUsers")
+
 
 router.get("/address/:countryName/:addressId", getUserAddressHandler)
 router.get("/", getAllUsersHandler)
@@ -23,7 +24,7 @@ router.post("/login", loginHandler)
 
 router.delete("/delete/:id", deleteUserHandler)
 router.put("/update/:id",updateUserHandler)
-
+router.put("/update/image/:id", updateUserImageHandler)
 router.post("/getToken", getToken)
 router.post("/validateToken", validateToken)
 
