0418edfd711960a8ab9b7688ba2f57fd97136fde
CristianR1992 <rodriguez.cristianezequiel@hotmail.com>
2023-07-05 21:12:16 +0100

    modal para filtros

--- Vite/src/components/Home/Home.jsx
+++ Vite/src/components/Home/Home.jsx
@@ -15,48 +15,53 @@ const Home = () => {
     const [isLoading, setIsLoading] = useState(true);
     const userData = useSelector((state) => state.user.userData);
     const array = useSelector((state) => state.products.productsSoD);
-    const concatenatedObjects = array.reduce((accumulator, currentArray) => {
-        return accumulator.concat(currentArray);
-    }, []);
-    let currentProducts = null
-    if (!userData?.isAdmin || !userData?.isSuperAdmin) {
-        currentProducts = concatenatedObjects.filter(
-            (product) => product.catalog_listing === true
-        );
-    } else if (userData?.isAdmin || userData?.isSuperAdmin) {
-        currentProducts = concatenatedObjects
-    }
+    const [hasProducts, setHasProducts] = useState(true);
+    const [currentProducts, setCurrentProducts] = useState([]);
+    
     const [currentPage, setCurrentPage] = useState(1);
     const [productsPerPage] = useState(15);
     const indexOfLastProduct = currentPage * productsPerPage;
     const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-    const paginatedProducts = currentProducts.slice(
-        indexOfFirstProduct,
-        indexOfLastProduct
-    );
+
+    useEffect(() => {
+        const concatenatedObjects = array.reduce((accumulator, currentArray) => {
+            return accumulator.concat(currentArray);
+        }, []);
+        
+        let filteredProducts;
+        if (!userData?.isAdmin || !userData?.isSuperAdmin) {
+            filteredProducts = concatenatedObjects.filter(
+                (product) => product.catalog_listing === true
+            );
+        } else if (userData?.isAdmin || userData?.isSuperAdmin) {
+            filteredProducts = concatenatedObjects
+        }
+
+        setCurrentProducts(filteredProducts);
+    }, [array, userData]);
+
+    useEffect(() => {
+        setHasProducts(currentProducts.length > 0);
+    }, [currentProducts]);
 
     const paginado = (pageNumber) => {
         setCurrentPage(pageNumber);
     };
 
-
     useEffect(() => {
         dispatch(loginUserLocal())
-
     }, [])
 
     useEffect(() => {
         dispatch(axiosAllProductsByCountries(productsCountry));
-    }, [dispatch, productsCountry,]);
+    }, [dispatch, productsCountry]);
 
     useEffect(() => {
         setIsLoading(true);
-
         dispatch(loginUserLocal())
         const timer = setTimeout(() => {
             setIsLoading(false);
         }, 2500);
-
         return () => {
             clearTimeout(timer);
         };
@@ -66,6 +71,11 @@ const Home = () => {
         return <LoaderLanding />;
     }
 
+    const paginatedProducts = currentProducts.slice(
+        indexOfFirstProduct,
+        indexOfLastProduct
+    );
+
     return (
         <div className={style.body}>
             <Filter
@@ -73,20 +83,26 @@ const Home = () => {
                 countryId={productsCountry}
             />
             <div className={style.main}>
-                <Products currentProducts={paginatedProducts} />
-                <Paginado
-                    key="paginado"
-                    productsPerPage={productsPerPage}
-                    products={currentProducts.length}
-                    paginado={paginado}
-                    currentProducts={paginatedProducts}
-                />
+            {hasProducts ? (
+                <>
+                    <Products currentProducts={paginatedProducts} />
+                    <Paginado
+                        key="paginado"
+                        productsPerPage={productsPerPage}
+                        products={currentProducts.length}
+                        paginado={paginado}
+                        currentProducts={paginatedProducts}
+                    />
+                </>
+            ) : (
+                <div className={style.error}>
+                    <h2>Ups!</h2>
+                    <p>No se encontraron productos con esos filtros.</p>
+                </div>
+            )}
             </div>
         </div>
-
-
     );
 };
 
-
-export default Home
+export default Home;
